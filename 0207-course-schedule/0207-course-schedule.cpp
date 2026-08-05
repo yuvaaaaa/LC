d016747f03ca785eaf9efed1e0fe0f652cfe37e0
Yuva Rajkumar Meshram <yuvameshram29@gmail.com>
2026-08-05 14:22:07 +0530

    Time: 8 ms (34.27%), Space: 20 MB (32.4%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int bfs(unordered_map<int, vector<int>> &adj, vector<int>& indeg,
+             queue<int>& q ) {
+        int count = 0;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            count++ ;
+            for (auto& it : adj[u]) {
+                indeg[it]--;
+                if (indeg[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return count;
+    } 
+
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        int n = numCourses;
+
+        unordered_map<int, vector<int>> adj;
+
+        for (int i = 0; i < pre.size(); i++) {
+            adj[pre[i][1]].push_back(pre[i][0]);
+        }
+
+        vector<int> indeg(n, 0);
+
+        for (auto& it : adj) {
+            for (auto x : it.second) {
+                indeg[x]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0)
+                q.push(i);
+        }
+
+        int a = bfs(adj, indeg, q);
+
+        if (a==n) return true ;
+        return false ;
+    }
+};
