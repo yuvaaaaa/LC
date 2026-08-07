89f0d4fee3e01c036773f7f0876a400edd614233
Yuva Rajkumar Meshram <yuvameshram29@gmail.com>
2026-08-07 14:39:56 +0530

    Time: 14 ms (60.18%), Space: 43.8 MB (64.35%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,56 +1,56 @@
 class Solution {
 public:
-    int count =0;
-    int search(vector<int>&par , int i ){
-        if(par[i]==i){
+    int count = 0;
+    int search(vector<int>& par, int i) {
+        if (par[i] == i) {
             return i;
         }
 
         int a = search(par, par[i]);
         par[i] = a;
-        return a ;
-
+        return a;
     }
-    void uni(vector<int>&par , vector<int>&rank , int i , int j){
+    void uni(vector<int>& par, vector<int>& rank, int i, int j) {
 
-        int pi = search(par ,i);
-        int pj = search(par ,j);
+        int pi = search(par, i);
+        int pj = search(par, j);
 
-        if(pi == pj ){
-            count ++ ;
-            return ;
+        if (pi == pj) {
+            count++;
+            return;
         }
-        if(rank[pi]== rank[pj]){
-            par[pi] = pj ;
-            rank[pj]++ ;
-        }
-        else if(rank[pi]> rank[pj]){
+        if (rank[pi] == rank[pj]) {
+            par[pi] = pj;
+            rank[pj]++;
+        } else if (rank[pi] > rank[pj]) {
             par[pj] = pi;
-        }
-        else{
+        } else {
             par[pi] = pj;
         }
-        return ;
+        return;
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        vector<int>par(n);
+        vector<int> par(n);
 
-        for(int i=0;i<n;i++){
-            par[i] =i;
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
         }
-        vector<int>rank(n,0);
+        vector<int> rank(n, 0);
 
-        for(int i =0;i<connections.size();i++){
-            uni(par,rank,connections[i][0],connections[i][1]);
+        for (int i = 0; i < connections.size(); i++) {
+            uni(par, rank, connections[i][0], connections[i][1]);
         }
-        unordered_set<int>st ;
-        for(int i=0;i<n;i++){
-            st.insert(search(par , par[i]));
+        if (connections.size() < n - 1)
+            return -1;
+        unordered_set<int> st;
+        for (int i = 0; i < n; i++) {
+            st.insert(search(par, par[i]));
         }
 
         int d = st.size();
-        if(count>= d-1) return d-1;
+        if (count >= d - 1)
+            return d - 1;
 
-        return -1 ;
+        return -1;
     }
 };
